db7795654b8038b2beb9ef06691065f4e61fd572
CAROL\Carol <carol.berpas@gmail.com>
2018-04-15 00:09:43 +0200

    SetKeyOutline()

--- Assets/Scripts/OutlineProjection.cs
+++ Assets/Scripts/OutlineProjection.cs
@@ -2,20 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//Key. follows player's light.
 public class OutlineProjection : MonoBehaviour {
-
+    
     private LineRenderer lineRenderer;
-    private Vector2[] testIntersections = new Vector2[3];
+    private Vector2[] intersections;
+    private Vector2[] testIntersections= new Vector2[3];
     public float distanceToOutline = 1.06f;
     // Use this for initialization
     void Start () {
+        //test points
         testIntersections[0] = new Vector2((float)-0.5097109, (float)3.74515);
         testIntersections[1] = new Vector2((float)-0.006582094, (float)3.996876);
         testIntersections[2] = new Vector2((float)0.5090174, (float)3.750584);
-
-        lineRenderer = GetComponent<LineRenderer>();
         
+        lineRenderer = GetComponent<LineRenderer>();
+        SetKeyOutline(testIntersections);
     }
+
+    public void SetKeyOutline(Vector2[] key)
+    {
+        intersections = new Vector2[key.Length];
+        for (int i = 0; i < intersections.Length; i++)
+        {
+            intersections[i] = key[i];
+        }
+    }
+
 	public void ProjectOutline(Vector2[] points)
     {
         lineRenderer.positionCount = points.Length;
@@ -29,13 +42,18 @@ public class OutlineProjection : MonoBehaviour {
 
     // Update is called once per frame
     void Update () {
-        for(int i=0; i < testIntersections.Length; i++)
+        //setting spheres at dots positions. for debugging.
+        /* for(int i=0; i < testIntersections.Length; i++)
+         {
+             GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+             Destroy(sphere, 0.01f);
+             sphere.transform.position = new Vector3(testIntersections[i].x, 0, testIntersections[i].y);
+             sphere.transform.localScale = Vector3.one * 0.1f;
+         }*/
+        if (intersections!=null)
         {
-            GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-            Destroy(sphere, 0.01f);
-            sphere.transform.position = new Vector3(testIntersections[i].x, 0, testIntersections[i].y);
-            sphere.transform.localScale = Vector3.one * 0.1f;
+            ProjectOutline(intersections);
         }
-        ProjectOutline(testIntersections);
+       
 	}
 }
